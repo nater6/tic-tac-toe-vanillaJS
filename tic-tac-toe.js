d33a83a02de00abc745c8e26558cdf7ddfc98716
nater68 <n.d.russell@hotmail.com>
2022-06-25 02:20:38 +0100

    Even listners added but not doing anything

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,6 +1,15 @@
 function pageLayout() {
-    window.addEventListener('load', e => {
-        console.log('Pageloaded')
+   // add event an event listner to the divs
+    // let box1 = document.getElementById('1')
+    // box1.addEventListener('click', e => {
+    //     console.log(1)
+    // }) 
+    const boxes = document.querySelectorAll('.gridbox')
+    boxes.forEach(box => {
+        box.addEventListener('click', e => {
+            console.log(box.id)
+            box.innerHTML = 'x'
+        })
     })
 }
 pageLayout()
